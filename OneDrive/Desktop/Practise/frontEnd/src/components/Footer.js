03569add1a153134d4320bcca37d9acf3a0c2a59
harshbadgujars <hardyalsinghdaudsar@gmail.com>
2023-03-11 08:47:08 +0530

    change some spell

--- OneDrive/Desktop/Practise/frontEnd/src/components/Footer.js
+++ OneDrive/Desktop/Practise/frontEnd/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='footer'>
       <h1 className='footer_heading'>HARSH</h1>
-      <q className='footer_qutes'><i>If you whant to be in the top 1% people don't do whant 99% people do</i></q>
+      <q className='footer_qutes'><i>If you whant to be in the top 1% people don't do what 99% people do</i></q>
       <p className='footer_copyright'>Copyright © 2022 hardyalsingh.com</p>
     </div>
   )
